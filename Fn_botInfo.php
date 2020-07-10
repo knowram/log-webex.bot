86be9e18b8441f1642e5c0818e81b0962392ee9d
knowram <jacob.munson@gmail.com>
2020-07-10 06:23:13 -0500

    Update Fn_botInfo.php

--- Fn_botInfo.php
+++ Fn_botInfo.php
@@ -1,16 +1,16 @@
 <?php
 
 define("DB_HOST", "localhost");
-define("DB_NAME", "u264166153_bots");
-define("DB_USER", "u264166153_botDB");
-define("DB_PASS", 'r7^b2}N2A2;cE@');
+define("DB_NAME", "db");
+define("DB_USER", "user");
+define("DB_PASS", 'pass');
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-$authId = "OWI4ZDBlYWEtNzM3MS00M2M1LWIzMTAtZDJiNmRmNGJlZTVjMDNkYTNmNTItZDQ3_PF84_consumer";
-$bogId = "Y2lzY29zcGFyazovL3VzL1BFT1BMRS8yZjNmYjVkYy0zZjhiLTRiYTMtODcwOS03MmE2NGE2Nzg0OWQ";
+$authId = "OWI4ZDBlYWEtNzM3MS00M2M1LWI....NmNTItZDQ3_PF84_consumer";
+$bogId = "Y2lzY29zcGFyazovL3Vz....zZjhiLTRiYTMtODcwOS03MmE2NGE2Nzg0OWQ";
 
 
-$client_id = 'Ccf1789f7bc9b9eda238d829b796a7b2a303008057945b89a4aedff76ba27789a';
-$Client_Secret = '9255855c73022b76ed8e311dbad4c44f2d18c3129eb220e5b14bd0c1f19692f3';
-$redirect_uri = urlencode('https://bots.wxteams.com/log/Fn_oAuth.php');
+$client_id = 'Ccf1789f7bc9b9eda238d8....3008057945b89a4aedff76ba27789a';
+$Client_Secret = '9255855c73022b76ed8e31.....20e5b14bd0c1f19692f3';
+$redirect_uri = urlencode('https://[your server]/log/Fn_oAuth.php');
 ?>
